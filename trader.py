4c7a54a07b6777053dfdc7518c7cdaf455556347
lakshandk44-a11y <lakshandk44@gmail.com>
2026-07-02 10:59:09 +0530

    Create trader.py

---
+++ trader.py
@@ -0,0 +1,22 @@
+import ccxt
+import pandas as pd
+import requests
+from config import *
+
+exchange = ccxt.binance({
+    "apiKey": BINANCE_API_KEY,
+    "secret": BINANCE_API_SECRET,
+    "options": {"defaultType": "future"}
+})
+
+def send(msg):
+    requests.post(DISCORD_WEBHOOK, json={"content": msg})
+
+
+def get_data(symbol, tf):
+    data = exchange.fetch_ohlcv(symbol, tf, limit=100)
+    return pd.DataFrame(data, columns=["t","o","h","l","c","v"])
+
+
+def trade(symbol, side):
+    return exchange.create_market_order(symbol, side.lower(), TRADE_SIZE)
